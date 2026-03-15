962db4b247e4a1b4c093694325f7fca734fa7448
omarhr-001 <omarharbaoui987@gmail.com>
2026-03-15 17:51:23 +0100

    first commit menu project

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useLanguage } from "@/hooks/useLanguage";
+
+
+export function Header() {
+  return (
+    
+    <header>
+ <img src="/img/logo1.png"  className="restaurant-logo" alt="Logo The Challenge" />
+
+      <div className="header-ornament">
+        <div className="ornament-line"></div>
+        <div className="ornament-sm"></div>
+        <div className="ornament-diamond"></div>
+        <div className="ornament-sm"></div>
+        <div className="ornament-line right"></div>
+        
+      </div>
+
+      <p className="restaurant-label fr">Là où les nuits prennent vie</p>
+      <p className="restaurant-label en">Where Nights Come Alive</p>
+
+      <h1 className="restaurant-name">
+        <em>THE</em> CHALLENGE
+      </h1>
+
+      <p className="restaurant-tagline fr">La nuit vous appartient</p>
+      <p className="restaurant-tagline en">The Night is Yours</p>
+
+      <div className="header-meta">
+        <span className="fr">
+          Ambiance <strong>Non-Stop</strong>
+        </span>
+        <span className="en">
+          Vibes <strong>No-Stop</strong>
+        </span>
+      </div>
+    </header>
+  );
+}
